c973a22597013b6c66dc2da20b8a9fc4c773a9c3
FelipeMovio <felipelsmovio@gmail.com>
2026-03-07 16:07:46 -0300

    create no item repository existsByNomeAndTipo

--- src/main/java/com/sistema_docao/demo/repository/ItemRepository.java
+++ src/main/java/com/sistema_docao/demo/repository/ItemRepository.java
@@ -1,7 +1,10 @@
 package com.sistema_docao.demo.repository;
 
 import com.sistema_docao.demo.entity.Item;
+import com.sistema_docao.demo.entity.TipoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    boolean existsByNomeAndTipo(String nome, TipoItem tipo);
 }
