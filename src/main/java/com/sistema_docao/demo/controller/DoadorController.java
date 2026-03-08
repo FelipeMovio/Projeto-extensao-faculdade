6fb8559f2b8f32bcac5ee61fe47328462c139ea5
FelipeMovio <felipelsmovio@gmail.com>
2026-03-07 23:32:35 -0300

    update

--- src/main/java/com/sistema_docao/demo/controller/DoadorController.java
+++ src/main/java/com/sistema_docao/demo/controller/DoadorController.java
@@ -1,16 +1,15 @@
 package com.sistema_docao.demo.controller;
 
 import com.sistema_docao.demo.dto.request.DoadorCadastroRequestDTO;
-import com.sistema_docao.demo.entity.Doador;
+import com.sistema_docao.demo.dto.response.DoadorReadResponseDTO;
 import com.sistema_docao.demo.service.DoadorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/doadores")
@@ -24,5 +23,12 @@ public class DoadorController {
     public ResponseEntity<Void> cadastrar(@Valid @RequestBody DoadorCadastroRequestDTO dto){
        doadorService.register(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DoadorReadResponseDTO>> verTodos(Pageable pageable){
+        Page<DoadorReadResponseDTO> dto = doadorService.getAll(pageable);
+
+        return ResponseEntity.ok(dto);
     }
 }
