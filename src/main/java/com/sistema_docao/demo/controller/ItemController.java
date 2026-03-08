ef28b4dc97f0fc8657684fb3b4428c97cff9f282
FelipeMovio <felipelsmovio@gmail.com>
2026-03-08 12:13:25 -0300

    PostMapping criar item feito

--- src/main/java/com/sistema_docao/demo/controller/ItemController.java
+++ src/main/java/com/sistema_docao/demo/controller/ItemController.java
@@ -1,7 +1,12 @@
 package com.sistema_docao.demo.controller;
 
+import com.sistema_docao.demo.dto.request.ItemCadastroRequestDTO;
 import com.sistema_docao.demo.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,12 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+
+    @PostMapping
+    public ResponseEntity<Void> criar(@RequestBody ItemCadastroRequestDTO dto){
+        itemService.registrar(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
 }
