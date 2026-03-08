e506b88448d70aba8f54d6d16ba77452f4cf2d00
FelipeMovio <felipelsmovio@gmail.com>
2026-03-08 12:20:46 -0300

    GetMapping de itens feito

--- src/main/java/com/sistema_docao/demo/controller/ItemController.java
+++ src/main/java/com/sistema_docao/demo/controller/ItemController.java
@@ -1,14 +1,15 @@
 package com.sistema_docao.demo.controller;
 
 import com.sistema_docao.demo.dto.request.ItemCadastroRequestDTO;
+import com.sistema_docao.demo.dto.response.ItemReadResponseDTO;
 import com.sistema_docao.demo.service.ItemService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/itens")
@@ -19,10 +20,24 @@ public class ItemController {
 
 
     @PostMapping
-    public ResponseEntity<Void> criar(@RequestBody ItemCadastroRequestDTO dto){
+    public ResponseEntity<Void> criar(@RequestBody @Valid ItemCadastroRequestDTO dto){
         itemService.registrar(dto);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ItemReadResponseDTO>> verTodos(Pageable pageable){
+        Page<ItemReadResponseDTO> dto = itemService.getAll(pageable);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemReadResponseDTO> verUm (@PathVariable Long id){
+        ItemReadResponseDTO dto = itemService.getOne(id);
+
+        return ResponseEntity.ok(dto);
     }
 
 }
