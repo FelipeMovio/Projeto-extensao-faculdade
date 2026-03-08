159e3f64eb5558269ece8b20a63861edefde69ee
FelipeMovio <felipelsmovio@gmail.com>
2026-03-08 12:09:02 -0300

    ...

--- src/main/java/com/sistema_docao/demo/controller/ItemController.java
+++ src/main/java/com/sistema_docao/demo/controller/ItemController.java
@@ -1,9 +1,16 @@
 package com.sistema_docao.demo.controller;
 
+import com.sistema_docao.demo.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/itens")
 public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+
 }
