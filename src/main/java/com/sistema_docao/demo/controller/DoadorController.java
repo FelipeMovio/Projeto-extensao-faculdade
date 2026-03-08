e48b0af598e07061af880bc4ca4f3c485ceedd0d
FelipeMovio <felipelsmovio@gmail.com>
2026-03-07 23:10:25 -0300

    inicando controller doador

---
+++ src/main/java/com/sistema_docao/demo/controller/DoadorController.java
@@ -0,0 +1,9 @@
+package com.sistema_docao.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doador")
+public class DoadorController {
+}
