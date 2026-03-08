a180c07d74604004be766511916f2419edaa9ed9
FelipeMovio <felipelsmovio@gmail.com>
2026-03-07 23:22:34 -0300

    update

--- src/main/java/com/sistema_docao/demo/controller/DoadorController.java
+++ src/main/java/com/sistema_docao/demo/controller/DoadorController.java
@@ -1,9 +1,25 @@
 package com.sistema_docao.demo.controller;
 
+import com.sistema_docao.demo.dto.request.DoadorCadastroRequestDTO;
+import com.sistema_docao.demo.entity.Doador;
+import com.sistema_docao.demo.service.DoadorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doador")
 public class DoadorController {
+
+    @Autowired
+    private DoadorService doadorService;
+
+    public ResponseEntity<Void> cadastrar(@Valid @RequestBody DoadorCadastroRequestDTO dto){
+       doadorService.register(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
