35c34cbf77552a3e418cc8f5dc0aab650bbabb46
FelipeMovio <felipelsmovio@gmail.com>
2026-03-09 18:03:58 -0300

    criando construtor da classe

--- src/main/java/com/sistema_docao/demo/controller/DoacoesController.java
+++ src/main/java/com/sistema_docao/demo/controller/DoacoesController.java
@@ -1,4 +1,17 @@
 package com.sistema_docao.demo.controller;
 
+import com.sistema_docao.demo.service.DoacoesService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doacoes")
 public class DoacoesController {
+
+    private final DoacoesService doacoesService;
+
+    public DoacoesController (DoacoesService doacoesService){
+        this.doacoesService = doacoesService;
+    }
+
 }
