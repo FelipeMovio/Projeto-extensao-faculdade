f40a4febc08b47c03a790f64703306037e36b002
FelipeMovio <felipelsmovio@gmail.com>
2026-03-10 22:04:11 -0300

    update

--- src/main/java/com/sistema_docao/demo/controller/RelatorioController.java
+++ src/main/java/com/sistema_docao/demo/controller/RelatorioController.java
@@ -1,4 +1,30 @@
 package com.sistema_docao.demo.controller;
 
+import com.sistema_docao.demo.dto.RelatorioItemDTO;
+import com.sistema_docao.demo.service.RelatorioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/relatorios")
 public class RelatorioController {
+
+    private final RelatorioService service;
+
+    public RelatorioController(RelatorioService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/mensal")
+    public List<RelatorioItemDTO> gerarRelatorio(
+            @RequestParam LocalDate inicio,
+            @RequestParam LocalDate fim
+    ) {
+        return service.gerarRelatorio(inicio, fim);
+    }
 }
