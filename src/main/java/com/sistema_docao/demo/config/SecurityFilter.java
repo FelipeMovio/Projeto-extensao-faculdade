7475ca522514b288b8dabc6d555971eb98dfccb4
FelipeMovio <felipelsmovio@gmail.com>
2026-03-15 12:20:48 -0300

    update

--- src/main/java/com/sistema_docao/demo/config/SecurityFilter.java
+++ src/main/java/com/sistema_docao/demo/config/SecurityFilter.java
@@ -1,4 +1,51 @@
 package com.sistema_docao.demo.config;
 
-public class SecurityFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private TokenConfig tokenConfig;
+
+
+    public SecurityFilter(TokenConfig tokenConfig) {
+        this.tokenConfig = tokenConfig;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (Strings.isNotEmpty(authHeader) && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            Optional<JWTUserData> optUser = tokenConfig.validateToken(token);
+
+            if (optUser.isPresent()) {
+                JWTUserData userData = optUser.get();
+
+                List<SimpleGrantedAuthority> authorities = userData.roles().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userData, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
 }
