82bc6f306d945cd2a512abeff06874aa0bc4a77c
FelipeMovio <felipelsmovio@gmail.com>
2026-03-15 11:58:34 -0300

    update

--- src/main/java/com/sistema_docao/demo/config/AuthConfig.java
+++ src/main/java/com/sistema_docao/demo/config/AuthConfig.java
@@ -1,11 +1,25 @@
 package com.sistema_docao.demo.config;
 
+import com.sistema_docao.demo.repository.UsuarioRepository;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Getter
 @Setter
-public class AuthConfig {
+public class AuthConfig implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    public AuthConfig(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
 }
