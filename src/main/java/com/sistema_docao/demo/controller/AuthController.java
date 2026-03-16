9d2f320e0786ef22735d457f696e8dc9a633029c
FelipeMovio <felipelsmovio@gmail.com>
2026-03-16 19:55:47 -0300

    update

--- src/main/java/com/sistema_docao/demo/controller/AuthController.java
+++ src/main/java/com/sistema_docao/demo/controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.sistema_docao.demo.controller;
 
+import com.sistema_docao.demo.config.TokenConfig;
+import com.sistema_docao.demo.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private UsuarioService service;
+
+
+    private AuthenticationManager authenticationManager;
+
+
+    private  TokenConfig tokenConfig;
+
+    public AuthController(UsuarioService service, AuthenticationManager authenticationManager, TokenConfig tokenConfig) {
+        this.service = service;
+        this.authenticationManager = authenticationManager;
+        this.tokenConfig = tokenConfig;
+    }
+
+
 }
