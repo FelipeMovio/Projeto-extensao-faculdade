060014e63efa603183a054e937d77121d363e31d
FelipeMovio <felipelsmovio@gmail.com>
2026-04-02 12:17:47 -0300

    alteraçoes e testes feitos no controller de doacoes

--- src/main/java/com/sistema_docao/demo/controller/DoacoesController.java
+++ src/main/java/com/sistema_docao/demo/controller/DoacoesController.java
@@ -4,10 +4,12 @@ import com.sistema_docao.demo.config.JWTUserData;
 import com.sistema_docao.demo.dto.sistema.request.DoacaoCadastroRequestDTO;
 import com.sistema_docao.demo.dto.sistema.response.DoacaoReadResponseDTO;
 import com.sistema_docao.demo.service.DoacoesService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,13 +26,14 @@ public class DoacoesController {
     @PostMapping
     public ResponseEntity<Void> doar(
             @AuthenticationPrincipal JWTUserData user,
-            @RequestBody DoacaoCadastroRequestDTO dto) {
+            @Valid @RequestBody DoacaoCadastroRequestDTO dto) {
 
         doacoesService.registrarDoacao(user.userId(), dto);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Page<DoacaoReadResponseDTO>> verTodas(Pageable pageable) {
 
@@ -39,6 +42,7 @@ public class DoacoesController {
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<DoacaoReadResponseDTO> verUma(@PathVariable Long id) {
 
